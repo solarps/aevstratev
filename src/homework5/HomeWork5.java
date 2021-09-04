4ffb03d6f030f49b7f5aaf64d51c459531200d21
solarps <evstratevandrey@gmail.com>
2021-09-04 15:48:07 +0300

    Fix id

--- src/homework5/HomeWork5.java
+++ src/homework5/HomeWork5.java
@@ -2,12 +2,13 @@ package homework5;
 
 public class HomeWork5 {
     public static void main(String[] args) {
-        HomeAnimal dog = new HomeAnimal(1, AnimalType.Dog, 5, 20, ColorType.Black, "Rex", true, true);
+        int id=0;
+        HomeAnimal dog = new HomeAnimal(++id, AnimalType.Dog, 5, 20, ColorType.Black, "Rex", true, true);
         System.out.println();
         System.out.println(dog);
         System.out.println(dog.getAnswer());
         dog.getHome();
-        WildAnimal wolf = new WildAnimal(2, AnimalType.Wolf, 6, 24, ColorType.Gray, true);
+        WildAnimal wolf = new WildAnimal(++id, AnimalType.Wolf, 6, 24, ColorType.Gray, true);
         System.out.println(wolf);
         System.out.println(wolf.getAnswer());
     }
